16c0529ff893908c7ca881e61cfbea62be5ecdbf
Ben Bryant <brbryant2639@gmail.com>
2020-02-28 18:36:09 -0600

    added progress bar animation

--- scripts.js
+++ scripts.js
@@ -1,38 +1,57 @@
+let card1 = document.getElementById('card1');
+let card2 = document.getElementById('card2');
+let card3 = document.getElementById('card3');
+let card4 = document.getElementById('card4');
+let card5 = document.getElementById('card5');
+let card6 = document.getElementById('card6');
+let card7 = document.getElementById('card7');
+let card8 = document.getElementById('card8');
+let card9 = document.getElementById('card9');
+
 slowMath.add(6, 2)
 .then((answer) => {
     console.log(answer);
+    card1.textContent = answer;
     return slowMath.multiply(answer, 2);
 })
 .then((answer) => {
     console.log(answer);
+    card2.textContent = answer;
     return slowMath.divide(answer, 4);
 })
 .then((answer) => {
     console.log(answer);
+    card3.textContent = answer;
     return slowMath.subtract(answer, 3);
 })
 .then((answer) => {
     console.log(answer);
+    card4.textContent = answer;
     return slowMath.add(answer, 98);
 })
 .then((answer) => {
     console.log(answer);
+    card5.textContent = answer;
     return slowMath.remainder(answer, 2);
 })
 .then((answer) => {
     console.log(answer);
+    card6.textContent = answer;
     return slowMath.multiply(answer, 50);
 })
 .then((answer) => {
     console.log(answer);
+    card7.textContent = answer;
     return slowMath.remainder(answer, 40);
 })
 .then((answer) => {
     console.log(answer);
+    card8.textContent = answer;
     return slowMath.add(answer, 32);
 })
 .then((answer) => {
     console.log(`The final result is ${answer}.`);
+    card9.textContent = `The final result is ${answer}.`;
 })
 .catch((err) => {
     console.log(err);
